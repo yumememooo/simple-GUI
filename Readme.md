1125558397565583917d0b55236705cefdc392fa
yumememooo <yumememooo@gmil.com>
2021-01-23 16:13:31 +0800

    Fix:get result json to table

--- Readme.md
+++ Readme.md
@@ -1,39 +1,70 @@
-
-
 #simple-html
 
+簡單使用 Javascript(JQuery)/css/html 搭建打後端 API 呈現與操作之 GUI
 
+## 簡介
 
+- html
+  畫面元素的搭建 <table class="table" id="table"> - bootstrap:網站樣板，由 HTML、CSS 和 JavaScript 寫成的前端框架，核心的設計目標是達成 RWD 響應式與行動優先。
+  如果不想要管網頁設計可以直接向以下這樣套用 ex:按鈕設計
+  https://www.w3schools.com/bootstrap/bootstrap_buttons.asp (w3school 提供了很多範例線上可以直接按套用)
+- css
+  畫面元素的樣式，色彩，動畫等
+- Javascript(js)
+  操控畫面的邏輯 - Jquery :jQuery 以 Javascript 來編寫的函式庫，先幫你實作了很多 Javascript 的函數功能，讓開發者可以更輕鬆方便操作網頁元素，是免費的，<head>需引入。 - jQuery 元素选择器:因為要操控 HTML 元素，就必須知道怎麼對應，最簡單的三種如下:
+  ` $("button") 選到HTML元素中 <button> 元素。 $("intro") 選到HTML元素中有加class="intro" 的元素。 $("#demo") 選到HTML元素中的 id="demo" 的 元素，正常應只有一個。 `
+  更多:更多:https://www.w3schools.com/jQuery/jquery_ref_selectors.asp - React - Vue - Anglar
 
-1.樣式simple 下拉框 按鈕等等 HTML
+## 實作參考
 
+- HTML 頁面參考 1.輸入框:下拉選單
+  https://www.w3schools.com/bootstrap4/bootstrap_forms_inputs.asp
 
-
-2.Juery 按鈕事件
+  2.Juery 按鈕事件
 
 jQuery click() Method
 https://www.w3schools.com/jquery/event_click.asp
 
-jQuery - ajax
--【jQuery】如何使用 ajax 和後端 server 進行資料的 request, response
+jQuery - ajax -【jQuery】如何使用 ajax 和後端 server 進行資料的 request, response
 https://spicyboyd.blogspot.com/2018/08/jquery-ajax-server-request-response.html
 
+3.Json to table
+JQuery 版本 Json to table
+https://www.w3schools.com/code/tryit.asp?filename=GHYGTEA5D18G
+https://ithelp.ithome.com.tw/questions/10199870
 
+### debug & Issue
 
+- CORS
+  如果打 API 遇到類似以下錯誤，代表跨域請求失敗，一般來講可以請後端加上'Access-Control-Allow-Origin':\*實作解決。
 
-## git at local initial setting
+```
+Access to XMLHttpRequest at 'http://127.0.0.1:56888/api/v1/pet?animal_kind=Mickey%20Mouse&_=1611386085016' from origin 'null' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+```
+
+### git at local initial setting
+
 - 1.link project
+
 ```
 git init
 git remote add origin https://github.com/yumememooo/simple-GUI.git
 ```
+
 - 2.check commit user
+
 ```
 //check local user
 git config user.name
 git config user.email
 //change local user
-git config user.name "yumememooo" 
+git config user.name "yumememooo"
 git config user.email "yumememooo@gmil.com"
-* 此為指定本專案，請勿加上--global全域設定 
+* 此為指定本專案，請勿加上--global全域設定
 ```
+
+Prettier - Code formatter - 自動排版
+Prettier - Code formatter
+對HTML/CSS/JS支援自動化format排版效果，提升美觀性容易維護。
+- vscode
+  settings-Format on save
--- custom/js/click.js
+++ custom/js/click.js
@@ -1,37 +1,62 @@
-$(document).ready(function(){
-    $("button").click(function(){ //mean select <button>
-      alert("Value: " + $("#test").val()); //mean select < id="test"> //alet會跳出視窗
-      getpets()
-    });
+$(document).ready(function () {
+  $("button").click(function () {
+    //mean select <button>
+    // alert("Value: " + $("#test").val()); //mean select < id="test"> //alet會跳出視窗
+
+    getpets($("#kind").val());
+  });
+});
+
+function getpets(kind) {
+  // 接收前端的input資料：帳號、密碼
+  //var data = { id: $("#account").val(), pwd: $("#password").val() };
+  var url = "http://localhost:56888/api/v1/pet?animal_kind=" + kind;
+
+  $.ajax({
+    url: url, //後端的URL
+    type: "GET", //用POST的方式
+    // dataType: "json", //response的資料格式
+    // cache: false, //是否暫存
+    // data: data, //傳送給後端的資料
+    success: function (response) {
+      console.log(response); //成功後回傳的資料json
+      alert(response);
+      $("#size").text(response.length); 
+      if (response.length > 0) {
+        jsonString = JSON.stringify(response); //JSON.stringify() 方法是將一個JavaScript值(物件或者陣列)轉換為一個 JSON字串
+        json2table(jsonString, $("#table"));
+      }
+    },
+  });
+}
+
+function json2table(jsonString, $table) {
+  var json = JSON.parse(jsonString);
+  var cols = Object.keys(json[0]);
+
+  var headerRow = "";
+  var bodyRows = "";
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  $table.html("<thead><tr></tr></thead><tbody></tbody>");
+
+  cols.map(function (col) {
+    headerRow += "<th>" + capitalizeFirstLetter(col) + "</th>";
   });
 
+  json.map(function (row) {
+    bodyRows += "<tr>";
 
-  function getpets() {
-    // 接收前端的input資料：帳號、密碼
-    //var data = { id: $("#account").val(), pwd: $("#password").val() };
-    $.ajax({
-        url: "http://127.0.0.1:56888/api/v1/pet",   //後端的URL
-        type: "GET",   //用POST的方式
-        dataType: "json",   //response的資料格式
-        cache: false,   //是否暫存
-       // data: data, //傳送給後端的資料
-        success: function(response) {
-            console.log(response);  //成功後回傳的資料
-
-            // if (response.auth === 1) {
-            //     window.location.href = "camera.html";   //帳密確認成功後，轉址
-            // } else {
-            //     $("#invalid").show();   //帳密輸入錯誤，訊息框
-            //     centered($("#invalid"));
-
-            //     $(window).resize(function() {
-            //         centered($("#invalid"));
-            //     });
-
-            //     setTimeout(function() {
-            //         $("#invalid").hide();
-            //     }, 3000);
-            // }
-        }
+    cols.map(function (colName) {
+      bodyRows += "<td>" + row[colName] + "</td>";
     });
+
+    bodyRows += "</tr>";
+  });
+
+  $table.find("thead tr").append(headerRow);
+  $table.find("tbody").append(bodyRows);
 }
